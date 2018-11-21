a5fec9cf58c9201ecb5139a9208059e662274aff
Morten Greger <mortro@donamaretto.de>
2018-11-21 14:13:15 +0100

    Revert "adding missing require for custom faraday adapter"
    This reverts commit 6990ed9f06a5ee07bf81b35f4087ac983ee2ef04.

--- lib/afterbuy.rb
+++ lib/afterbuy.rb
@@ -1,7 +1,6 @@
 require 'active_support/configurable'
 require 'active_support/inflector'
 require 'faraday'
-require 'faraday/adapter/afterbuy_http'
 require 'ostruct'
 require 'roar/decorator'
 require 'roar/xml'
