1dc3e7d5cdde473f69124b7fc967b8c4ba3aa1bb
Morten Greger <mortro@donamaretto.de>
2018-11-29 17:16:09 +0100

    make shop interface work

--- lib/afterbuy/api.rb
+++ lib/afterbuy/api.rb
@@ -50,47 +50,52 @@ module Afterbuy
     private
 
     def post(body, options = {})
-      url = options.delete(:api_type) == :shop_interface ? @shop_interface_url : @api_url
+      args = if (options.delete(:api_type) == :shop_interface)
+        [@shop_interface_url, { form: body }]
+      else
+        [@api_url, { body: body }]
+      end
+
       http = HTTP
       options.each do |key, value|
         http = http.public_send(key, value)
       end
 
       self.debug_info = { request_params: body }
-      response = http.post url, body: body
+      response = http.post *args
       self.debug_info[:response_body] = response.body.to_s
 
       raise APIError, response.body.to_s unless response.status.success?
       response
     end
 
-      def request_params(method_name, global_params={}, payload={})
-        request_params = payload.merge({
-          afterbuy_global: Global.new(
-            global_params.merge({
-              partner_id: @partner_id,
-              partner_password: @partner_password,
-              user_id: @user_id,
-              user_password: @user_password,
-              call_name: method_name,
-              detail_level: 0,
-              error_language: 'EN'
-            })
-          )
-        })
+    def request_params(method_name, global_params={}, payload={})
+      request_params = payload.merge({
+        afterbuy_global: Global.new(
+          global_params.merge({
+            partner_id: @partner_id,
+            partner_password: @partner_password,
+            user_id: @user_id,
+            user_password: @user_password,
+            call_name: method_name,
+            detail_level: 0,
+            error_language: 'EN'
+          })
+        )
+      })
 
-        method_request      = "Afterbuy::#{METHOD_REQUEST_MAPPING[method_name]}Request".constantize.new(request_params)
-        request_representer = "Afterbuy::Representer::#{METHOD_REQUEST_MAPPING[method_name]}RequestRepresenter".constantize.new(method_request)
-        return CGI.unescape_html(request_representer.to_xml)
-      end
+      method_request      = "Afterbuy::#{METHOD_REQUEST_MAPPING[method_name]}Request".constantize.new(request_params)
+      request_representer = "Afterbuy::Representer::#{METHOD_REQUEST_MAPPING[method_name]}RequestRepresenter".constantize.new(method_request)
+      return CGI.unescape_html(request_representer.to_xml)
+    end
 
-      def shop_interface_request_params(global_params={}, request=Afterbuy::ShopInterfaceRequest.new)
-        request.partner_id = global_params[:partner_id] || @partner_id
-        request.partner_pass = global_params[:partner_pass] || @partner_password
-        request.user_id = global_params[:user_id] || @user_id
+    def shop_interface_request_params(global_params={}, request=Afterbuy::ShopInterfaceRequest.new)
+      request.partner_id = global_params[:partner_id] || @partner_id
+      request.partner_pass = global_params[:partner_pass] || @partner_password
+      request.user_id = global_params[:user_id] || @user_id
 
-        Afterbuy::Representer::ShopInterfaceRequestRepresenter.new request
-      end
+      Afterbuy::Representer::ShopInterfaceRequestRepresenter.new request
+    end
 
   end
 end
